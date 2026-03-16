d07c3ed38fe94e2d5a4704cffe3aca74d51bdf7f
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2026-03-16 16:29:17 -0300

    Refactor sequential search program structure

--- exercicios/semana-3/03_sequencial.cpp
+++ exercicios/semana-3/03_sequencial.cpp
@@ -3,47 +3,46 @@
 
 #define TAM 1000000
 
-int* carregarArquivo(){								//funcao para carregar o txt 
-
+//funcao para carregar o txt 
+int* carregarArquivo(){								
     FILE *arq = fopen("dados.txt","r");
-
+    
     if(arq == NULL){
         printf("Erro ao abrir o arquivo\n");
-        exit(1); 		//encerra o programa
+        exit(1); //encerra o programa
     }
 
-    int *v = (int*) malloc(TAM * sizeof(int));		//v passa a apontar p o primeiro elemento do vetor
+    //v passa a apontar p o primeiro elemento do vetor
+    int *v = (int*) malloc(TAM * sizeof(int));		
 
     if(v == NULL){
-        printf("Erro de alocação de memoria\n");
+        printf("Erro de alocaÃ§Ã£o de memoria\n");
         exit(1);
     }
-
+    
+    //le o arquivo e guarda na posicao i do vetor
     for(int i = 0; i < TAM; i++){
-        fscanf(arq,"%d",&v[i]);						//le o arquivo e guarda na posicao i do vetor
+        fscanf(arq,"%d",&v[i]);
     }
-
     fclose(arq);
-
-    return v;										//retorna o ponteiro p o vetor criado
+    return v; //retorna o ponteiro p o vetor criado
 }
 
-int buscaSequencial(int v[], int n, int chave, int *acessos){		//vetor, tamanho do vetor, valor procurado, contador de acessos
+//vetor, tamanho do vetor, valor procurado, contador de acessos
+int buscaSequencial(int v[], int n, int chave, int *acessos){
 
-    for(int i = 0; i < n; i++){					//verifica elemento por elemento
+    //verifica elemento por elemento
+    for(int i = 0; i < n; i++){	
         (*acessos)++;
 
         if(v[i] == chave)
-            return i;						//retorna o indice da posicao
+            return i; //retorna o indice da posicao
     }
-
-    return -1;								//se nao encontra
+    return -1; //se nao encontra
 }
 
 int main(){
-
-    int *v = carregarArquivo();			//chama a funcao que le o arquivo
-
+    int *v = carregarArquivo();	//chama a funcao que le o arquivo
     int valores[] = {7341488,85,265654,732765,8313596,45744};
     int qtdvalores = 6;
     int totalAcessos = 0;
@@ -51,19 +50,12 @@ int main(){
     printf("Busca sequencial\n");
 
     for(int i = 0; i < qtdvalores; i++){
-
-        int acessos = 0;				//zera os acessos a cada vez (pode ser até 1000000)
-
+        int acessos = 0; //zera os acessos a cada vez (pode ser atÃ© 1000000)
         buscaSequencial(v, TAM, valores[i], &acessos);
-        
         totalAcessos = totalAcessos + acessos;
-
         printf("Valor %d - %d acessos\n", valores[i], acessos);
     }
-    
     printf("Total de acessos: %d", totalAcessos);
-    
     free(v);
-
     return 0;
 }
