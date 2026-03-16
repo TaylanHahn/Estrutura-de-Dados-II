c491138548bed233510a4c44521b1fba657b44f3
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2026-03-16 16:27:09 -0300

    Refactor binary search program in C

--- exercicios/semana-3/03_binaria.cpp
+++ exercicios/semana-3/03_binaria.cpp
@@ -3,8 +3,8 @@
 
 #define TAM 1000000
 
-int* carregarArquivo(){								//funcao para carregar o txt 
-
+//funcao para carregar o txt 
+int* carregarArquivo(){								
     FILE *arq = fopen("dados.txt","r");
 
     if(arq == NULL){
@@ -12,20 +12,22 @@ int* carregarArquivo(){								//funcao para carregar o txt
         exit(1); 		//encerra o programa
     }
 
-    int *v = (int*) malloc(TAM * sizeof(int));		//v passa a apontar p o primeiro elemento do vetor
+    //v passa a apontar p o primeiro elemento do vetor
+    int *v = (int*) malloc(TAM * sizeof(int));		
 
     if(v == NULL){
-        printf("Erro de alocação de memoria\n");
+        printf("Erro de alocaÃ§Ã£o de memoria\n");
         exit(1);
     }
 
+    //le o arquivo e guarda na posicao i do vetor
     for(int i = 0; i < TAM; i++){
-        fscanf(arq,"%d",&v[i]);						//le o arquivo e guarda na posicao i do vetor
+        fscanf(arq,"%d",&v[i]);						
     }
 
     fclose(arq);
 
-    return v;										//retorna o ponteiro p o vetor criado
+    return v; //retorna o ponteiro p o vetor criado
 }
 
 int buscaBinaria(int v[], int n, int chave, int *acessos){
@@ -33,18 +35,19 @@ int buscaBinaria(int v[], int n, int chave, int *acessos){
     int inicio = 0;
     int fim = n - 1;
 
-    while(inicio <= fim){					//enquanto o intervalo for valido
+    //enquanto o intervalo for valido
+    while(inicio <= fim){					
 
         int meio = (inicio + fim) / 2;
-        (*acessos)++;						//a cada meio, contabiliza um acesso
+        (*acessos)++; //a cada meio, contabiliza um acesso
 
         if(v[meio] == chave)
             return meio;
 
-        if(chave < v[meio])				//se for menor, elimina toda metade direita, desloca pra esquerda
+        if(chave < v[meio])	//se for menor, elimina toda metade direita, desloca pra esquerda
             fim = meio - 1;
         else
-            inicio = meio + 1;			//senao, elimina a metade esquerda e desloca pra direita
+            inicio = meio + 1;	//senao, elimina a metade esquerda e desloca pra direita
     }
 
     return -1;
@@ -52,7 +55,7 @@ int buscaBinaria(int v[], int n, int chave, int *acessos){
 
 int main(){
 
-    int *v = carregarArquivo();			//chama a funcao que le o arquivo
+    int *v = carregarArquivo();	//chama a funcao que le o arquivo
 
     int valores[] = {7341488,85,265654,732765,8313596,45744};
     int qtdvalores = 6;
@@ -61,13 +64,9 @@ int main(){
     printf("\nBusca binaria\n");
 
     for(int i = 0; i < qtdvalores; i++){
-
-        int acessos = 0;			//zera os acessos a cada vez 
-
+        int acessos = 0; //zera os acessos a cada vez 
         buscaBinaria(v, TAM, valores[i], &acessos);
-        
         totalAcessos = totalAcessos + acessos;
-
         printf("Valor %d - %d acessos\n", valores[i], acessos);
     }
     
